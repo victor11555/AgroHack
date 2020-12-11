d482bab15bf6c55ceee24c46fb9410919c0382af
Fira Salimov <fira9494@mail.ru>
2020-12-12 02:22:46 +0300

    Firs route

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,15 @@
+import { Switch, Route } from 'react-router-dom';
+
+
 function App() {
+
   return (
     <>
+      <Switch>
+        <Route path=''>
+
+        </Route>
+      </Switch>
     </>
   );
 }
