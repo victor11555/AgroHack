4b3ce8441775fc6f267100ffaa735f8f196aa7cc
Fira Salimov <fira9494@mail.ru>
2020-12-12 02:11:47 +0300

    Add Components for consumer and Supplier

---
+++ front/src/Consumer/ConsumerLoginForm/ConsumerLoginForm.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function ConsumerLoginForm() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
---
+++ front/src/Supplier/SupplierSignUpForm/SupplierSignUpForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SupplierSignUpForm() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
